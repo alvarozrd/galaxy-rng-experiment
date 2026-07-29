3ceae7e41e02cdf6cf53145df725a1cd0f06d25a
Álvaro Pereira <alvarozord@gmail.com>
2026-07-29 16:13:17 -0300

    Add canonical PCG64-CP A Lite 1 core implementation

--- src/rng/--init--.py
+++ src/rng/--init--.py
@@ -1,3 +1,7 @@
 """
-Implementações de geradores pseudoaleatórios utilizadas no projeto.
+Geradores pseudoaleatórios utilizados no Galaxy RNG Experiment.
 """
+
+from .core import PCG64CPALite1Core
+
+__all__ = ["PCG64CPALite1Core"]
